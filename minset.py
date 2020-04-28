70f6f72b6cdd9ec48e557bfd74a58f9c83ee22ce
Raghav1705 <raghavsingh1705@gmail.com>
2020-04-29 01:22:08 +0530

    improved starter code

---
+++ minset.py
@@ -0,0 +1,18 @@
+"""
+CS 170 Project : Spring 2020
+Avik Sethia, Cassidy Hardin, Raghav Singh
+An attempt to solve an NP-Hard Problem of min-set cover with the added constraint that the cover must be connected.
+
+The algorithm returns a valid solution that is not guaranteed to the be the optimum.
+The randomness ensures that the algorithm is not guaranteed to return the same solution on each run.
+Therefore, it can run repeatedly and the minimum solution is picked as the optimum.
+This is the best approximation of the actual solution.
+"""
+
+def minset():
+    return None
+
+def connected():
+    return True
+
+def 
