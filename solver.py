b9730a4e09cbab2ab8cfe887920bd1d89607c1d8
cassidyhardin <cassidyhardin@berkeley.edu>
2020-05-01 14:30:04 +0900

    remerged dijkstras implementation algorithim

--- solver.py
+++ solver.py
@@ -11,7 +11,6 @@ def dijkstraSet(G):
     """
     Args:
         G: networkx.Graph
-
     Returns:
         T: networkx.Graph
     """
@@ -20,9 +19,92 @@ def dijkstraSet(G):
     cities = set()
     vertexSet = set()
     vertexSet.update(G.nodes)
-    degreeSort = sorted(G.degree, key=lambda x: x[1], reverse=True)g
-    # TODO: your code here!
-    return RajivMishraAlgorithm(G)
+    degreeSort = sorted(G.degree, key=lambda x: x[1], reverse=True)
+  
+
+    maximum = degreeSort[0][0]
+    T.add_node(maximum)
+    towers.add(maximum)
+    vertexSet.remove(maximum)
+    for v in G.neighbors(maximum):
+        cities.add(v)    
+
+    while len(vertexSet) != 0:
+        start = random.sample(vertexSet, 1)
+        value = math.inf
+        node = []
+        for s, end, weight in G.edges(start, data=True):
+            if end in cities:
+                for city, tower, cost in G.edges(end, data=True):
+                    if tower in towers:
+                        temp = T.copy()
+                        temp.add_edge(tower, city, weight=cost['weight'])
+                        currCost = average_pairwise_distance(temp)
+                        if currCost < value:
+                            node = []
+                            value = currCost
+                            test = (city, tower, cost)
+                            node.append(test)
+            for c in cities:
+                common = nx.common_neighbors(G, c, end)
+                if common is not None:
+                    for n in common:
+                        edgeNode = G.get_edge_data(end, n)
+                        for middle, city, cost1 in G.edges(n, data=True):
+                            # find corresponding city
+                            if city in cities:
+                                for connection, tower, cost2 in G.edges(city, data=True):
+                                    if tower in towers:
+                                        temp = T.copy()
+                                        temp.add_edge(n, end, weight=edgeNode['weight'])
+                                        temp.add_edge(n, city, weight=cost1['weight'])
+                                        temp.add_edge(tower, city, weight=cost2['weight'])
+                                        currCost = average_pairwise_distance(temp)
+                                        if currCost < value:
+                                            value = currCost
+                                            node = []
+                                            test = (end, n, edgeNode)
+                                            node.append(test)
+                                            test1 = (n, city, cost1)
+                                            node.append(test1)
+                                            test2 = (city, tower, cost2)
+                                            node.append(test2)
+        if node is not None:  
+            for pair in node:
+                begin = pair[0]
+                end = pair[1]
+                weight = pair[2]
+                cycleTest = T.copy()
+                cycleTest.add_edge(begin, end, weight=weight['weight'])
+                if not nx.is_tree(cycleTest):
+                    break
+                T.add_edge(begin, end, weight=weight['weight'])
+                if begin in cities: 
+                    cities.remove(begin)
+                if end in cities:
+                    cities.remove(end)
+                towers.add(begin)
+                towers.add(end)
+                if begin in vertexSet: 
+                    vertexSet.remove(begin)
+                if end in vertexSet:
+                    vertexSet.remove(end)
+                for v in G.neighbors(begin):
+                    if v not in towers: 
+                        if v not in cities:
+                            cities.add(v)
+                for v in G.neighbors(end):
+                    if v not in towers:
+                        if v not in cities:
+                            cities.add(v)
+            if start[0] in vertexSet:
+                vertexSet.remove(start[0])
+                if start[0] not in cities:
+                    cities.add(start[0])
+            # print(vertexSet)
+            # print(T.nodes)
+    return T
+
 # Here's an example of how to run your solver.
 
 # Usage: python3 solver.py test.in
@@ -118,6 +200,6 @@ if __name__ == "__main__":
     for input_path in os.listdir(input_dir):
         graph_name = input_path.split(".")[0]
         G = read_input_file(f"{input_dir}/{input_path}")
-        T = solver(G)
+        T = dijkstraSet(G)
         #print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
         write_output_file(T, f"{output_dir}/{graph_name}.out")
