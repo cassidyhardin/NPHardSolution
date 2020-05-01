d8c12993cbf0daef1010a3a9510d74b2670656f5
Avik Sethia <asethia@cloudknox.io>
2020-04-30 23:48:22 -0700

    Pruning + Additional conditioning before vertices are added to vertex set, Rank 45 with an avg of 38

--- solver.py
+++ solver.py
@@ -1,13 +1,118 @@
 import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import is_valid_network, average_pairwise_distance,average_pairwise_distance_fast
-import sys
+
+import networkx as nx
+from parse import read_input_file, read_output_file, write_output_file
+from utils import is_valid_network, average_pairwise_distance
 import os
-import matplotlib.pyplot as plt
+import heapq
+import math
+import os
+import random
+
+
 def one_by_one(s, l):
     for i in l:
         s.add(i)
 
+
+def dijkstraSet(G):
+    """
+    Args:
+        G: networkx.Graph
+    Returns:
+        T: networkx.Graph
+    """
+    T = nx.Graph()
+    towers = set()
+    cities = set()
+    vertexSet = set()
+    vertexSet.update(G.nodes)
+    degreeSort = sorted(G.degree, key=lambda x: x[1], reverse=True)
+
+    maximum = degreeSort[0][0]
+    T.add_node(maximum)
+    towers.add(maximum)
+    vertexSet.remove(maximum)
+    for v in G.neighbors(maximum):
+        cities.add(v)
+
+    while len(vertexSet) != 0:
+        start = random.sample(vertexSet, 1)
+        value = math.inf
+        node = []
+        for s, end, weight in G.edges(start, data=True):
+            if end in cities:
+                for city, tower, cost in G.edges(end, data=True):
+                    if tower in towers:
+                        temp = T.copy()
+                        temp.add_edge(tower, city, weight=cost['weight'])
+                        currCost = average_pairwise_distance(temp)
+                        if currCost < value:
+                            node = []
+                            value = currCost
+                            test = (city, tower, cost)
+                            node.append(test)
+            for c in cities:
+                common = nx.common_neighbors(G, c, end)
+                if common is not None:
+                    for n in common:
+                        edgeNode = G.get_edge_data(end, n)
+                        for middle, city, cost1 in G.edges(n, data=True):
+                            # find corresponding city
+                            if city in cities:
+                                for connection, tower, cost2 in G.edges(city, data=True):
+                                    if tower in towers:
+                                        temp = T.copy()
+                                        temp.add_edge(n, end, weight=edgeNode['weight'])
+                                        temp.add_edge(n, city, weight=cost1['weight'])
+                                        temp.add_edge(tower, city, weight=cost2['weight'])
+                                        currCost = average_pairwise_distance(temp)
+                                        if currCost < value:
+                                            value = currCost
+                                            node = []
+                                            test = (end, n, edgeNode)
+                                            node.append(test)
+                                            test1 = (n, city, cost1)
+                                            node.append(test1)
+                                            test2 = (city, tower, cost2)
+                                            node.append(test2)
+        if node is not None:
+            for pair in node:
+                begin = pair[0]
+                end = pair[1]
+                weight = pair[2]
+                cycleTest = T.copy()
+                cycleTest.add_edge(begin, end, weight=weight['weight'])
+                if not nx.is_tree(cycleTest):
+                    break
+                T.add_edge(begin, end, weight=weight['weight'])
+                if begin in cities:
+                    cities.remove(begin)
+                if end in cities:
+                    cities.remove(end)
+                towers.add(begin)
+                towers.add(end)
+                if begin in vertexSet:
+                    vertexSet.remove(begin)
+                if end in vertexSet:
+                    vertexSet.remove(end)
+                for v in G.neighbors(begin):
+                    if v not in towers:
+                        if v not in cities:
+                            cities.add(v)
+                for v in G.neighbors(end):
+                    if v not in towers:
+                        if v not in cities:
+                            cities.add(v)
+            if start[0] in vertexSet:
+                vertexSet.remove(start[0])
+                if start[0] not in cities:
+                    cities.add(start[0])
+            # print(vertexSet)
+            # print(T.nodes)
+    return T
 def solve(G):
     """
     Args:
@@ -16,14 +121,14 @@ def solve(G):
     Returns:
         T: networkx.Graph
     """
+    #rm_solution= dijkstraSet(G)
+    G.remove_edges_from(nx.selfloop_edges(G))
     for node in G.nodes():
         if G.degree(node)>=len(G.nodes)-1:
             print("This Edge is connected to all other edges")
             ret_graph=nx.Graph()
             ret_graph.add_node(node)
             return ret_graph
-
-
     mst_prim = modified_mst(G,'prim')
     mst_kruskal = modified_mst(G, 'kruskal')
     mst_boruvka= modified_mst(G, 'boruvka')
@@ -32,7 +137,9 @@ def solve(G):
     for a, b, data in sorted(solution.edges(data=True), key=lambda x: x[2]['weight']):
         print('{a} {b} {w}'.format(a=a, b=b, w=data['weight']))
 
+    #if average_pairwise_distance_fast(solution)<=average_pairwise_distance_fast(rm_solution):
     return solution
+
 
 
 
@@ -91,9 +198,15 @@ def modified_mst(graph,algorithm):
                 big_sub_graph = max(nx.connected_component_subgraphs(mst_copy_2), key=len)
                 if vertices_covered_by_tree(big_sub_graph,graph) and average_pairwise_distance_fast(big_sub_graph)<distance_to_beat:
                     one_by_one(removed_nodes,list(small_sub_graph.nodes()))
-                    print("GRAPH HAS BEEN PRUNED")
+                    print("SMALL GRAPH HAS BEEN PRUNED")
                     mst_copy_2=big_sub_graph
                     mst_copy=big_sub_graph
+
+                elif vertices_covered_by_tree(small_sub_graph,graph) and average_pairwise_distance_fast(small_sub_graph)<distance_to_beat:
+                    one_by_one(removed_nodes, list(big_sub_graph.nodes()))
+                    print("BIG GRAPH HAS BEEN PRUNED")
+                    mst_copy_2 = small_sub_graph
+                    mst_copy = small_sub_graph
                 else:
                     mst_copy_2.add_edge(a, b,weight=data['weight'])
 
@@ -108,12 +221,26 @@ def modified_mst(graph,algorithm):
 
                 if is_leaf(mst_copy, a) and not (is_leaf(mst_copy, b)) and (a not in vertex_set) and vertices_covered_by_tree(mst_copy_2,graph) :
                     # print("Removing this edge and node part a")
-                    vertex_set.add(b)
+                    check_copy=mst_copy.copy()
+                    check_copy.remove_node(a)
+                    check_copy.remove_node(b)
+
+                    if not node_in_vertices_covered_by_tree(a,check_copy,graph):
+                        vertex_set.add(b)
+                    else:
+                        print(" B DIDN'T GET ADDED")
                     mst_copy.remove_edge(a, b)
                     mst_copy.remove_node(a)
                 elif is_leaf(mst_copy, b) and not (is_leaf(mst_copy, a)) and (b not in vertex_set) and vertices_covered_by_tree(mst_copy_2,graph):
                     # print("Removing this edge and node part b")
-                    vertex_set.add(a)
+                    check_copy = mst_copy.copy()
+                    check_copy.remove_node(a)
+                    check_copy.remove_node(b)
+
+                    if not node_in_vertices_covered_by_tree(b,check_copy, graph):
+                        vertex_set.add(a)
+                    else:
+                        print(" A DIDN'T GET ADDED")
                     mst_copy.remove_edge(a, b)
                     mst_copy.remove_node(b)
                 else:
@@ -142,6 +269,14 @@ def vertices_covered_by_tree(tree,graph):
             vertice_set.add(neighbor)
             vertice_set.add(node)
     return len(vertice_set)==len(graph.nodes())
+
+def node_in_vertices_covered_by_tree(node,tree,graph):
+    vertice_set=set()
+    for node in tree.nodes():
+        for neighbor in graph.neighbors(node):
+            vertice_set.add(neighbor)
+            vertice_set.add(node)
+    return node in vertice_set
 # Here's an example of how to run your solver.
 
 # Usage: python3 solver.py test.in
@@ -154,7 +289,6 @@ if __name__ == '__main__' :
         print(graph_name)
         G = read_input_file(f"{input_dir}/{input_path}")
         T = solve(G)
-        nx.draw(T)
         write_output_file(T, f"{output_dir}/{graph_name}.out")
 
 
